509607a6ec3d45539eab3723ff48f91aa28f27c2
Fernando <fergimenodj@gmail.com>
2015-05-21 11:36:08 +0200

    Creada plantilla base

--- Proyecto_Final/src/main/java/daw/ed/proyect_fgimeno/App.java
+++ Proyecto_Final/src/main/java/daw/ed/proyect_fgimeno/App.java
@@ -1,5 +1,13 @@
 package daw.ed.proyect_fgimeno;
 
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import java.util.*;
+import static spark.Spark.*;
+import spark.template.freemarker.FreeMarkerRoute;
+import spark.ModelAndView;
+import spark.Spark;
 /**
  * Hello world!
  *
@@ -8,6 +16,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+            Spark.staticFileLocation("/public");
+
+        get(new FreeMarkerRoute("/") {
+            @Override
+            public ModelAndView handle(Request request, Response response) {
+
+                return modelAndView(null, "listaUsuarios.ftl");
+            }
+        });
+    
     }
 }
---
+++ Proyecto_Final/src/main/resources/spark/template/freemarker/listaUsuarios.ftl
@@ -0,0 +1,27 @@
+<#include "plantilla.ftl">
+
+<#macro content>
+
+<table class="table table-hover">
+	<thead>
+		<tr>
+
+			<th>Nombre</th>  <th>Autor</th> <th>Album</th> <th>Duración</th> <th>Acciones</th>
+
+		</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<td>Nombre1</td> <td>Autor1</td> <td>Album1</td> <td>Duración1</td> <td>Accion</td>
+	</tr>
+	<tr>
+		<td>Nombre1</td> <td>Autor1</td> <td>Album1</td> <td>Duración1</td> <td>Accion</td>
+	</tr>
+	</tbody>
+</table>
+
+</#macro>
+
+
+
+<@display_page/>
